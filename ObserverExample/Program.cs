dd358f523c89993cc33f93280006d5825b0a39b3
Jens <Jens>
2016-04-03 19:54:01 +0200

    Added

---
+++ ObserverExample/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ObserverExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define a provider and two observers.
+            LocationTracker provider = new LocationTracker();
+            LocationReporter reporter1 = new LocationReporter("FixedGPS");
+            reporter1.Subscribe(provider);
+            LocationReporter reporter2 = new LocationReporter("MobileGPS");
+            reporter2.Subscribe(provider);
+
+            provider.TrackLocation(new Location(47.6456, -122.1312));
+            reporter1.Unsubscribe();
+            provider.TrackLocation(new Location(47.6677, -122.1199));
+            provider.TrackLocation(null);
+            provider.EndTransmission();
+        }
+    }
+}
